7fe606f5a5def0f7947738d4070ef3d6a56b358d
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-11-07 09:34:51 -0300

    Set up Express server and configure environment variables

---
+++ server/src/app.ts
@@ -0,0 +1,9 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+app.use(express.json());
+
+export default app;

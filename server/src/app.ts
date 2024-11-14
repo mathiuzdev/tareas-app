af8ecedd9878778d5477eaa04ab2591bd48d084d
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-11-13 23:19:46 -0300

    Update CORS configuration to allow requests from multiple origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:3000"], 
   credentials: true, //
 };
 

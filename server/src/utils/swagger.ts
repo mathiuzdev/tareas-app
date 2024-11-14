786a9d43c7d1e0e8d5c4ba63720f702387ddea89
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-11-13 21:43:34 -0300

    Refactor Swagger configuration: improve security scheme definition and adjust API paths based on environment

--- server/src/utils/swagger.ts
+++ server/src/utils/swagger.ts
@@ -3,43 +3,43 @@ import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 const options: swaggerJsdoc.Options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Task Manager API",
-        version: "1.0.0",
-        description: "API to manage tasks and tags, with JWT authentication.",
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task Manager API",
+      version: "1.0.0",
+      description: "API to manage tasks and tags, with JWT authentication.",
+    },
+    security: [
+      {
+        bearerAuth: [],
       },
-      security: [
-        {
-          bearerAuth: [],
-        },
-      ],
-      components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT",
-          },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
-    apis: ["./src/routes/*.ts"], 
-  };
-
-  
+  },
+  apis:
+    process.env.NODE_ENV !== "development"
+      ? ["./dist/routes/*.js"]
+      : ["./src/routes/*.ts"],
+};
 
 const swaggerSpec = swaggerJsdoc(options);
 
- function swaggerDocs(app: Express, port: number){
-    app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    app.get("/docs.json", (_: Request, res: Response) => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(swaggerSpec);
-        console.log(`Swagger JSON served at http://localhost:${port}/docs.json`);
-    });
-
+function swaggerDocs(app: Express, port: number) {
+  app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/docs.json", (_: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+    console.log(`Swagger JSON served at http://localhost:${port}/docs.json`);
+  });
 }
 
 export default swaggerDocs;

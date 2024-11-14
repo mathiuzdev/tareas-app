9f62a03d54cfea99d00a40e6233cefe4033f4944
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-11-13 21:26:42 -0300

    Enhance server startup: parse PORT from environment and integrate Swagger documentation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
+import swaggerDocs  from "./utils/swagger";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  swaggerDocs(app, PORT);
 });
